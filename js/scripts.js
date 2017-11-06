b1a6bc094141330d23b7bfdbe248ac15d1ed83b2
Alexei Kutyrev <alexei.kutyrev@gmail.com>
2017-11-07 00:38:16 +0300

    ---

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
 'use strict';
 
 class TicTacToe {
-    constructor () {
-        console.log('fds');
+    constructor (el) {
+        this.el = document.getElementById(el);
+        this.init();
+        this.state = {};
+    }
+
+    init () {
+        for (let i = 0; i < 3; i++) {
+            this.el.innerHTML += `<div class="row" id="row_${(i + 1)}"></div>`;
+            for (let j = 0; j < 3; j++) {
+                document.getElementById(`row_${(i + 1)}`).innerHTML += `<div class="cell" id="cell_${(i + 1)}">${(i + 1)}</div>`;
+            }
+        }
     }
 }
 
-const ticTacToe = new TicTacToe();
+const ticTacToe = new TicTacToe('app');
 
